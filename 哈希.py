8139a7c58574b4e2a5aa289004bc13ba2455f9bc
Chubby Long <91040760+SeaBreezing@users.noreply.github.com>
2024-02-09 18:45:38 +0800

    Update 哈希.py

--- 哈希.py
+++ 哈希.py
@@ -16,3 +16,34 @@ class Solution(object):
             if(record[i] != 0):
                 return False
         return True
+
+# 快乐数
+# https://leetcode.cn/problems/happy-number/
+class Solution(object):
+    def cal(self, n):
+        result = 0
+        if (n > 10):
+            while(n//10):# 取余数
+                result += (n % 10)**2
+                print(f'result={result}')
+                n = n//10 # 地板除
+        result += n ** 2
+        return result
+    
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # 如果集合中出现了这个数，那么会进入无限循环，该数不是快乐数
+        record = set() # create a set
+        if n <= 0:
+            return False
+        while(n != 1):
+            n = self.cal(n)
+            if n in record:
+                return False
+            else:
+                record.add(n)
+        return True
+
