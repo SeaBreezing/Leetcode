abed6c48fc974f9a1bd0e051a9a20114f30a197c
Chubby Long <91040760+SeaBreezing@users.noreply.github.com>
2024-04-29 10:56:42 +0800

    Update 贪心.py

--- 贪心.py
+++ 贪心.py
@@ -25,6 +25,7 @@ class Solution:
 
 # 最大子数组和
 # https://leetcode.cn/problems/maximum-subarray/description/
+# 贪心的思路为局部最优：当前“连续和”为负数的时候立刻放弃，从下一个元素重新计算“连续和”，因为负数加上下一个元素 “连续和”只会越来越小。从而推出全局最优：选取最大“连续和”
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         result = float('-inf')
